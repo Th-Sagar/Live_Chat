2d461511d150f5dc8e07a2738abd554888eed36b
Th-Sagar <sagarthapa98122@gmail.com>
2024-04-29 06:59:46 +0545

    feat:validation

--- Server/router/auth-router.js
+++ Server/router/auth-router.js
@@ -6,10 +6,7 @@ import {
 } from "../controller/auth-controller.js";
 
 const router = express.Router();
-
 router.route("/register").post(userController);
 router.route("/login").post(loginController);
-
 router.route("/users/:userId").get(readUsers);
-
 export default router;
